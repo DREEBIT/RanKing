68a1878014f3dc6fbbd31df7c70776399579747a
Matthias Schmidt <matthias.schmidt@dreebit.com>
2016-01-22 15:12:58 +0100

    MAR-14

--- modules/records/server/controllers/bot.server.controller.js
+++ modules/records/server/controllers/bot.server.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-console.log('running ...');
+console.log('Starting Bot ...');
 
 var request = require('request');
 var _ = require('underscore');
@@ -15,112 +15,98 @@ var allKeywords = require('../../../../config/keywords.json');
 
 module.exports = {
 
-  fetchKeywordIndizes: [],
+	fetchKeywordIndizes: [],
 
-  settings: {},
+	settings: {},
 
-  fetchKeyword: function(callback){
+	fetchKeyword: function(callback){
 
-    var me = this;
-  	if (this.fetchKeywordIndizes.length > 0){
-  		var fetchIndex = this.fetchKeywordIndizes.shift();
+		var me = this;
+		if (this.fetchKeywordIndizes.length > 0){
+			var fetchIndex = this.fetchKeywordIndizes.shift();
+			var index = fetchIndex % allKeywords.length;
+			this.fetchPage(index,0,function(){
+				me.fetchKeyword(callback);
+			});
+		}else {
+			if (typeof callback === 'function'){
+				callback();
+			}
+		}
+	},
 
+	fetchPage: function(keywordIndex, index, callback){
 
-  		var index = fetchIndex % allKeywords.length;
-  		this.fetchPage(index,0,function(){
-  			me.fetchKeyword(callback);
-  		});
-  	}else {
-  		if (typeof callback === 'function'){
-  			callback();
-  		}
-  	}
+		var me = this;
+		var keyword = allKeywords[keywordIndex];
+		var url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&q=' + keyword + '&start=' + index;
 
-  },
+		console.log('[Request]');
+		console.log('Keyword: '+keyword);
+		console.log('Page: '+index);
+		console.log('--------------------');
 
-  fetchPage: function(keywordIndex, index, callback){
+		request(url, function (error, response, body) {
 
-    var me = this;
-  	var keyword = allKeywords[keywordIndex];
-  	var url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&q=' + keyword + '&start=' + index;
+			if (!error && response.statusCode === 200) {
 
-  	console.log('[Request]');
-  	console.log('Keyword: '+keyword);
-  	console.log('Page: '+index);
-  	console.log('--------------------');
+				var json = JSON.parse(body);
+				if (json.responseData !== null) {
 
-  	request(url, function (error, response, body) {
+					var results = json.responseData.results;
+					_.each(results, function(element, i){
+						var results = json.responseData.results;
 
-  		if (!error && response.statusCode === 200) {
+						var e = new Record({
+							date: date,
+							keyword: keyword,
+							rank: index + i + 1,
+							link: element.url
+						});
 
-  			var json = JSON.parse(body);
-  			if (json.responseData !== null) {
+						e.save(function (err, obj) {
+							if (err) console.log('error while saving');
+						});
 
-          var results = json.responseData.results;
-          _.each(results, function(element, i){
-            var results = json.responseData.results;
+					});
+				}
+			} else {
+				console.log('Got an error: ", error, ", status code: ', response.statusCode);
+			}
 
-              var e = new Record({
-                date: date,
-                keyword: keyword,
-                rank: index + i + 1,
-                link: element.url
-              });
+			if (++index < me.settings.pageLimit){
+				me.fetchPage(keywordIndex, index, callback);
+			}else {
 
-              e.save(function (err, obj) {
-                if (err) console.log('error while saving');
-              });
+				me.settings.start = keywordIndex+1;
+				if (me.settings.start > allKeywords.length){
+					me.settings.start = 0;
+				}
 
+				fs.writeFile('./config/settings.json', JSON.stringify(me.settings), function(err){
+					if (err){
+						console.log(err);
+					}
+					if (typeof callback === 'function'){
+						callback();
+					}
+				});
+			}
+		});
+	},
 
-          });
+	start: function(){
 
-  			}
+		console.log('Start Requesting');
+		this.settings = JSON.parse(fs.readFileSync('./config/settings.json', 'utf8'));
+		this.fetchKeywordIndizes = _.range(this.settings.start, this.settings.start+this.settings.keywordsPerRun);
+		this.fetchKeyword(function(){
+			console.log('Done');
+		});
+	},
 
-  		} else {
-  			console.log('Got an error: ", error, ", status code: ', response.statusCode);
-  		}
+	log: function(message){
 
-  		if (++index < me.settings.pageLimit){
-  			me.fetchPage(keywordIndex, index, callback);
-  		}else {
-
-
-  			me.settings.start = keywordIndex+1;
-  			if (me.settings.start > allKeywords.length){
-  				me.settings.start = 0;
-  			}
-
-  			fs.writeFile('./config/settings.json', JSON.stringify(me.settings), function(err){
-          if (err){
-            console.log(err);
-          }
-  				if (typeof callback === 'function'){
-  					callback();
-  				}
-  			});
-
-
-
-  		}
-
-  	});
-
-
-  },
-
-
-  start: function(){
-
-    console.log('Start');
-    this.settings = JSON.parse(fs.readFileSync('./config/settings.json', 'utf8'));
-    this.fetchKeywordIndizes = _.range(this.settings.start, this.settings.start+this.settings.keywordsPerRun);
-    this.fetchKeyword(function(){
-      console.log('Done');
-    });
-
-  },
-
-  log: function(message){
-    console.log(message);
-  }
+		console.log(message);
+	}
 };
--- server.js
+++ server.js
@@ -6,33 +6,37 @@
 var app = require('./config/lib/app');
 var server = app.start();
 
-
-console.log('STARTING BOT EVERY 3 HOURS');
-
 var bot = require('./modules/records/server/controllers/bot.server.controller');
+/*
 var later = require('later');
-
 later.date.localTime();
 
-var schedule = {
+/!*var schedule = {
     schedules:[
-        {h: [0 ,3, 6 ,9, 12, 15, 18, 21]}
+        {m: [0 ,3, 6 ,9, 12, 15, 18, 21]}
     ]
+};*!/
+
+var schedule = {
+    schedules:
+        [
+            {m_b: [10]}
+        ]
 };
+
+console.log(schedule);
 
 later.setInterval(bot.start(), schedule);
 
-/*
+bot.start();*/
+
 console.log('STARTING CRON JOB EVERY 3 HOURS');
 
 var CronJob = require('cron').CronJob;
-//Alle 3 Stunden
-new CronJob('* * *!/3 * * *', function() {
+
+new CronJob('00  0,3,6,9,12,15,18,21  * * *', function() {
 
     console.log('Starting Cron Job');
     bot.start();
 
 }, null, true, 'Europe/Berlin');
-
-bot.start();
-*/
