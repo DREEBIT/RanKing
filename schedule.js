e110111048de8e817577aef96f88c649178e1fd0
Matthias Schmidt <matthias.schmidt@dreebit.com>
2016-01-26 14:42:05 +0100

    no message

---
+++ schedule.js
@@ -0,0 +1,14 @@
+/*
+var bot = require('./app/bot');
+
+var CronJob = require('cron').CronJob;
+
+//Alle 3 Stunden
+new CronJob('* * *!/3 * * *', function() {
+
+  bot.start();
+
+}, null, true, 'Europe/Berlin');
+
+bot.start();
+*/
